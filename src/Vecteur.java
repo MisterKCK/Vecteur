7fcd9e02ce300b4c514dedab59556a6dda66ad87
Karim Claude <ckarim2002@gmail.com>
2024-03-13 09:52:26 -0400

    Création du constructeur vecteur
    ajoute de la méthode resize

--- src/Vecteur.java
+++ src/Vecteur.java
@@ -1,12 +1,26 @@
 public class Vecteur {
-    private int[] Tableau;
-    private  int NbElement;
+    private int[] tableau;
+    private  int nbElement;
     public int RATION_AGRANDISSEMENT = 2;
-    public int TAILLE_INITIALE = 5;
+    public int TAILLE_INITIALE = 0;
 
-    public Vecteur() {
-        Tableau = new int[TAILLE_INITIALE];
-        NbElement = 0;
+    public Vecteur(int taille_initiale) {
+        tableau = new int[taille_initiale];
+        nbElement = 0;
+    }
+    public void ajoute(int element) {
+        if (nbElement >= tableau.length) {
+            resize();
+            tableau[TAILLE_INITIALE++] = element;
+        }
+    }
+
+    private void resize() {
+        int[] nouveau = new int[RATION_AGRANDISSEMENT * tableau.length];
+        for (int i = 0; i < nbElement; i++) {
+            nouveau[i] = tableau[i];
+        }
+        tableau = nouveau;
     }
 
 }
