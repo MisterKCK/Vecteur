8f2596af705747250631af77c4df6a2887b5293f
Karim Claude <ckarim2002@gmail.com>
2024-03-13 11:02:10 -0400

    Création du constructeur vecteur
    ajout de la méthode resize
    
    ajout du toString
    
    ajout de la méthode ajoute
    
    ajout getter nbElement et getElementAt
    
    ajout méthode estVide
    
    ajout méthode boolean ajoute

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,40 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("test 1: Création d'un vecteur vide");
         Vecteur data = new Vecteur();
-        data.ajouter(10);
+        System.out.println(data);
+        System.out.println("nbElement: " + data.getNbElement());
+        System.out.println("estVide:" + data.estVide());
+
+        System.out.println("test 2: Ajoute d'éléments à la fin du vecteur");
+        data.ajoute(0);
+        data.ajoute(10);
+        data.ajoute(20);
+        System.out.println(data);
+        System.out.println("nbElement: " + data.getNbElement());
+        System.out.println("estVide:" + data.estVide());
+
+        System.out.println("test 3: Ajouts d'élément jusqu'a ce que la capacité du vecteur augmente");
+        data.ajoute(30);
+        data.ajoute(60);
+        data.ajoute(90);
+        System.out.println(data);
+
+        System.out.println("test 4: vérifier l'état du vecteur");
+        System.out.println("idx0 : " + data.getElementAt(0));
+        System.out.println("idx3 : " + data.getElementAt(3));
+        System.out.println("idx5 : " + data.getElementAt(5));
+        System.out.println(data);
+        System.out.println("nbElement: " + data.getNbElement());
+        System.out.println("estVide:" + data.estVide());
+
+        System.out.println("test 5: Ajouts d'éléments à des positions spécifiques");
+        //data.ajoute(-10,0);
+        data.ajoute(25, 3);
+        //data.ajoute(60, 7);
+        System.out.println(data);
+        System.out.println("nbElement: " + data.getNbElement());
+        System.out.println("estVide:" + data.estVide());
 
     }
 }
--- src/Vecteur.java
+++ src/Vecteur.java
@@ -1,22 +1,40 @@
+import javax.swing.plaf.PanelUI;
+
 public class Vecteur {
     private int[] tableau;
     private int nbElement;
-    public int RATION_AGRANDISSEMENT = 2;
-    public int TAILLE_INITIALE = 5;
+    private static final int RATION_AGRANDISSEMENT = 2;
+    private static final int TAILLE_INITIALE = 5;
 
     public Vecteur() {
-        tableau = new int[TAILLE_INITIALE];
+        this(TAILLE_INITIALE);
+    }
+
+    public Vecteur(int taille_initiale) {
+        tableau = new int[taille_initiale];
         nbElement = 0;
     }
-    public int getNbElement() {
-        return nbElement;
-    }
+
     public void ajoute(int element) {
         if (nbElement <= tableau.length) {
             resize();
             tableau[nbElement++] = element;
         }
     }
+
+    public boolean ajoute(int element, int index) {
+        if (nbElement <= tableau.length) {
+            resize();
+        }
+        for (int i = nbElement; i > index; i--) {
+            tableau[i] = tableau[i - 1];
+        }
+        tableau[index] = element;
+        nbElement++;
+        return false;
+    }
+
+
     @Override
     public String toString() {
         String str = "[";
@@ -24,6 +42,17 @@ public class Vecteur {
             str += tableau[i] + " , ";
         }
         return str + "]";
+    }
+    public int getNbElement() {
+        return nbElement;
+    }
+
+    public int getElementAt(int index) {
+        return tableau[index];
+    }
+
+    public boolean estVide() {
+        return nbElement == 0;
     }
 
     private void resize() {
