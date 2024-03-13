7a1516ce0e600fb53c7a66f2c67abab74a599dd9
Karim Claude <ckarim2002@gmail.com>
2024-03-13 09:21:18 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Vecteur data = new Vecteur();
+        data.ajouter(10);
+
+    }
+}
---
+++ src/Vecteur.java
@@ -0,0 +1,12 @@
+import javax.swing.plaf.PanelUI;
+
+public class Vecteur {
+    private int[] mTableau;
+    private  int NbElement;
+    public int RATION_AGRANDISSEMENT = 2;
+    public int TAILLE_INITIALE = 5;
+
+    public Vecteur() {
+
+    }
+}
