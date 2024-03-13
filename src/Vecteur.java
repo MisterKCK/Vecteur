740e1bd2df9bbdef824c9d5ab624caf85a40a494
Karim Claude <ckarim2002@gmail.com>
2024-03-13 09:44:04 -0400

    Création du constructeur vecteur

--- src/Vecteur.java
+++ src/Vecteur.java
@@ -1,12 +1,12 @@
-import javax.swing.plaf.PanelUI;
-
 public class Vecteur {
-    private int[] mTableau;
+    private int[] Tableau;
     private  int NbElement;
     public int RATION_AGRANDISSEMENT = 2;
     public int TAILLE_INITIALE = 5;
 
     public Vecteur() {
-
+        Tableau = new int[TAILLE_INITIALE];
+        NbElement = 0;
     }
+
 }
